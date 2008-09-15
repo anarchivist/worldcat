a2d8cb73e3eae25e3995d47eed7bb8b66490f730
matienzo <none@none>
2008-09-15 21:48:59 +0000

    changed URL for project homepage in setup.py
    --HG--
    extra : convert_revision : svn%3A9bbe1849-123c-0410-9313-83ec163a5258/python/worldcat/trunk%40558

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ Development Status :: 1 - Planning
 setup( 
     name = 'worldcat',
     version = '0.0.5',  # remember to update worldcat/__init__.py on release!
-    url = 'http://svn.matienzo.org/public/python/worldcat',
+    url = 'http://matienzo.org/project/worldcat',
     author = 'Mark A. Matienzo',
     author_email = 'mark@matienzo.org',
     license = 'GPL',
