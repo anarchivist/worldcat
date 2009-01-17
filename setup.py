e0eb5284e8bf469888270daad5577ffaa37b638f
matienzo <none@none>
2009-01-17 08:54:28 +0000

    committing changes pre 0.2.1 tag
    --HG--
    extra : convert_revision : svn%3A9bbe1849-123c-0410-9313-83ec163a5258/python/worldcat/trunk%40592

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ Development Status :: 3 - Alpha
 
 setup( 
     name = 'worldcat',
-    version = '0.2.0',  # remember to update worldcat/__init__.py on release!
+    version = '0.2.1',  # remember to update worldcat/__init__.py on release!
     url = 'http://matienzo.org/project/worldcat',
     author = 'Mark A. Matienzo',
     author_email = 'mark@matienzo.org',
