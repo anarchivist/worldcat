601f059bb848b531bebc6141886173d91f8c8e56
matienzo <none@none>
2009-01-19 04:00:33 +0000

    committing latest version ofexample code
    --HG--
    extra : convert_revision : svn%3A9bbe1849-123c-0410-9313-83ec163a5258/python/worldcat/trunk%40594

--- examples/holdingsmap/holdingsmap.py
+++ examples/holdingsmap/holdingsmap.py
@@ -1,8 +1,7 @@
 """
-mark matienzo 2008
+mark matienzo 2009
 
-dependencies: web.py, lxml, worldcat, lxml, geopy
-
+dependencies: web.py, worldcat, simplejson, geopy, & elementtree
 to run: python holdingsmap.py
 point your web browser at http://localhost:8080/
 """
@@ -12,18 +11,20 @@ from worldcat.request.xid import xOCLCNUMRequest
 
 import simplejson
 import web
-from lxml import etree as ET
+from elementtree import ElementTree as ET
 from urllib2 import urlopen, HTTPError
 
 from geopy import geocoders as gc
 
-
-
-urls = ('/', 'index', '/locations/(.*)', 'locations', '/locations', 'locations', '/json/(.*)', 'json', '/(.*)__history__.html', 'history')
+urls = (
+        '/', 'index',
+        '/locations', 'locations',
+        '/json', 'json',
+        '/(locations|json)?__history__.html', 'history'
+        )
 render = web.template.render('templates/')
-WSKEY = 'YOUR API KEY'
-gmapkey = 'YOUR API KEY FOR GMAPSA'
-
+WSKEY = 'YOUR WORLDCAT API KEY'
+GMAPKEY = 'YOUR GOOGLE MAPS API KEY'
 class QGoogleGC(gc.Google):
     """docstring for QGoogleGC"""
     def __init__(self, api_key=None, domain='maps.google.com',
@@ -36,16 +37,19 @@ class QGoogleGC(gc.Google):
         dispatch = getattr(self, 'parse_' + self.output_format)
         return dispatch(page, exactly_one)
 
-gcoder  = QGoogleGC(gmapkey)
+gcoder  = QGoogleGC(GMAPKEY)
 
 class index:
     def GET(self):
-        print render.index(gmapkey=gmapkey)
-        
+        rdata = {'key': GMAPKEY, 'ctr': ('40.77','-73.98')}
+        print render.index(rdata=rdata)
+
 class json:
-    def GET(self, oclcnum):
-        jsonout = {'items': [], 'types': {'library': {'pluralLabel': 'libraries'}}}
-        lookup = LibrariesRequest(wskey=WSKEY, rec_num=oclcnum, maximumLibraries=100, ip='67.99.185.2').get_response()
+    def GET(self):
+        args = web.input(oclcnum=None, zip='11216')
+        jsonout = {'items': [],
+                    'types': {'library': {'pluralLabel': 'libraries'}} }
+        lookup = LibrariesRequest(wskey=WSKEY, rec_num=args.oclcnum, location=args.zip, maximumLibraries=100).get_response()
         results = ET.XML(lookup.data)
         for result in results.findall('holding'):
             _ = {}
@@ -67,37 +71,47 @@ class json:
         web.header('Content-Type', 'application/json')
         print simplejson.dumps(jsonout)
 
-
 class locations:
-    def GET(self, oclcnum):
-        citation = CitationRequest(wskey=WSKEY, rec_num=oclcnum).get_response().data
-        o = xOCLCNUMRequest(rec_num=oclcnum).get_response().data
-        others = []
+    def GET(self):
+    	rdata = web.input(oclcnum=None, zip='11216')
+    	c, rdata['ctr'] = gcoder.geocode(rdata['zip'])
+    	rdata['key'] = GMAPKEY
+        rdata['cit'] = CitationRequest(wskey=WSKEY, rec_num=rdata['oclcnum']).get_response().data
+        o = xOCLCNUMRequest(rec_num=rdata['oclcnum']).get_response().data
+        rdata['others'] = []
         try:
             for _ in o['list']:
-                others.extend(_['oclcnum'])
-            others.remove(oclcnum)
+                rdata['others'].extend(_['oclcnum'])
+            rdata['others'].remove(rdata['oclcnum'])
         except:
             pass
-        print render.locations(gmapkey=gmapkey, oclcnum=oclcnum, citation=citation, others=others)
+	    c, (lat, lon) = gcoder.geocode(rdata['zip'])
+        print render.locations(rdata=rdata)
     
     def POST(self):
-        oclcnum = web.input().oclcnum
-        citation = CitationRequest(wskey=WSKEY, rec_num=oclcnum).get_response().data
-        o = xOCLCNUMRequest(rec_num=oclcnum).get_response().data
-        others = []
+    	rdata = web.input(oclcnum=None, zip='11216')
+    	c, rdata['ctr'] = gcoder.geocode(rdata['zip'])
+    	rdata['key'] = GMAPKEY
+        rdata['cit'] = CitationRequest(wskey=WSKEY, rec_num=rdata['oclcnum']).get_response().data
+        o = xOCLCNUMRequest(rec_num=rdata['oclcnum']).get_response().data
+        rdata['others'] = []
         try:
             for _ in o['list']:
-                others.extend(_['oclcnum'])
-            others.remove(oclcnum)
+                rdata['others'].extend(_['oclcnum'])
+            rdata['others'].remove(rdata['oclcnum'])
         except:
             pass
-        print render.locations(gmapkey=gmapkey, oclcnum=oclcnum, citation=citation, others=others)
+	    c, (lat, lon) = gcoder.geocode(rdata['zip'])
+        print render.locations(rdata=rdata)
         
 class history:
-    def GET(self):
+    def GET(self, _=None):
         print '<html><body></body></html>'
 
-web.webapi.internalerror = web.debugerror
+def runfcgi_apache(func):
+    web.wsgi.runfcgi(func, None)
+      
 if __name__ == '__main__':
-    web.run(urls, globals(), web.reloader)
+    web.wsgi.runwsgi = runfcgi_apache
+    web.run(urls, globals())
+
